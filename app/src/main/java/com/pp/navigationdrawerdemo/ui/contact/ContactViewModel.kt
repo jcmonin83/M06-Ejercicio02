f9b29a8e16162b726bcf9d21c854a88075b39efc
jcmonin83 <jose.monin@gmail.com>
2022-04-16 11:24:38 -0500

    Initial commit

---
+++ app/src/main/java/com/pp/navigationdrawerdemo/ui/contact/ContactViewModel.kt
@@ -0,0 +1,13 @@
+package com.pp.navigationdrawerdemo.ui.contact
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel(){
+    private  val _text = MutableLiveData<String>().apply {
+        value = "Contact Fragment"
+    }
+
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/pp/navigationdrawerdemo/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.pp.navigationdrawerdemo.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
